749df68e6f6079d65c3372762df3634455d6f53c
OwenAnderson <owen_anderson@brown.edu>
2024-07-11 20:45:20 -0700

    pulling

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import SideNav from "@/app/ui/dashboard/sidenav";
+
+// receives a "children" property, which can either be a page or another layout
+// would not need to do all the type checking in javascript
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,30 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
+import AcmeLogo from "@/app/ui/acme-logo";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import styles from "@/app/ui/home.module.css";
+import { lusitana } from "@/app/ui/fonts";
+import Image from "next/image";
 
+// home page associated with the route "/"
+  // handle paths using the file system rather than a router directly
+  // allows for "colocation" --> basically, any files that deal with the same page in the same directory, and not split up across a component directory and a page directory
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+      <div
+        className={
+          "flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52"
+        }
+      >
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+          <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
+          <p
+            className={`${lusitana.className} antialiasedtext-xl text-gray-800 md:text-3xl md:leading-normal `}
+          >
+            <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
@@ -26,6 +39,27 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            // awesome, handles all of the scaling on mobile
+            // path to image
+            src="/hero-desktop.png"
+            // should be identical to the actual image size
+            width={1000}
+            height={760}
+            // removes the image from the DOM on mobile screens
+            className="hidden md:block"
+            alt={"Screenshots of the dashboard project showing desktop version"}
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            // removes the from the DOM on desktop screens
+            // note: a web font is a font in a website that isn't already installed in a user's device
+              // next is nice because it loads all of these before the page is rendered, and stores them, meaning no fear of performance issues
+            className="block md:hidden"
+            alt={"Screenshots of the dashboard project showing mobile version"}
+          />
         </div>
       </div>
     </main>
